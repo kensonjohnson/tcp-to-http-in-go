46ed5884a6b05b73ec9632220672c3eb17204147
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2025-04-14 08:49:37 -0500

    Add headers and tests

---
+++ internal/headers/headers_test.go
@@ -0,0 +1,71 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParse(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers.Get("Host"))
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Valid single header with extra whitespace
+	headers = NewHeaders()
+	data = []byte("     Content-Type: application/json        \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "application/json", headers.Get("Content-Type"))
+	assert.Equal(t, 45, n)
+	assert.False(t, done)
+
+	// Valid 2 headers with existing headers
+	headers = NewHeaders()
+	headers.Set("Host", "localhost:42069")
+	headers.Set("Content-Type", "application/json")
+	data = []byte("Authorization: Bearer ABCD\r\nAccept: application/json\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "Bearer ABCD", headers.Get("Authorization"))
+	assert.Equal(t, 28, n)
+	assert.False(t, done)
+	copy(data, data[28:])
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "application/json", headers.Get("Accept"))
+	assert.Equal(t, 26, n)
+	assert.False(t, done)
+
+	// Valid done
+	copy(data, data[26:])
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid header key (field-name)
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+}
